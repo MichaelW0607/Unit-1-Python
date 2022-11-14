6e0df0396c702f719cf846a9fce6eb71027c1dc5
MichaelW0607 <113442619+MichaelW0607@users.noreply.github.com>
2022-11-14 10:56:11 -0500

    added statements

---
+++ grades.py
@@ -0,0 +1,12 @@
+x = 100
+x=int("what is your grade?")
+if x >= 90 and x <= 99:
+    print("A")
+elif x >= 80 and x <= 89:
+    print("B")
+elif x >= 70 and x <= 79:
+    print("C")
+elif x >= 60 and x <= 69:
+    print("D")
+elif x >= 50 and x <= 59:
+    print("F")
