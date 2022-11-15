46ccfa918edfdc7285df4dc0924628d2f4bdc924
MichaelW0607 <113442619+MichaelW0607@users.noreply.github.com>
2022-11-15 10:52:28 -0500

    added loops and lists

---
+++ lists.py
@@ -0,0 +1,13 @@
+my_list = [ "cars","sports","food","music","excercises"]
+print(my_list)
+my_list.append("drinks")
+print(my_list)
+print(my_list[3])
+print(my_list[-1])
+my_list[0] = "movies"
+print(my_list)
+del my_list[2]
+print(my_list)
+my_tuple = ("pizza","hotdogs","tacos","steak","soup")
+# creating my tuple list
+print(my_tuple)
