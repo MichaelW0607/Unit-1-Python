b3eb609665af43ac6b0f1b110685acbaf011ada4
MichaelW0607 <113442619+MichaelW0607@users.noreply.github.com>
2022-11-15 10:52:57 -0500

    added todo and  lists

---
+++ loops.py
@@ -0,0 +1,17 @@
+while True:
+    print("hello world")
+    #creating my first while loop
+x = ""
+while x != "stop":
+    print("hello world")
+    x = input("Type stop to exit")
+    y = 1 
+    
+    while y <= 10:
+        print(y)
+        y = y + 1
+        animal = ["dogs","cats","turtles","lions","tigers"]
+        for z in animals:
+            print(z)
+        for i in range(1,11):
+            print(i)
---
+++ todo.py
@@ -0,0 +1,10 @@
+todos = []
+x= input("What to add to list?")
+todos.append(x)
+print("Your todo list is")
+print(todos) 
+while True:
+ print("What to add to list?")
+x = ""
+print("your todo list is ")
+x = input("What to add to list?")
