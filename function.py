b0621b15c62280b35ef50df93e0cae99695ff4cf
MichaelW0607 <113442619+MichaelW0607@users.noreply.github.com>
2022-11-16 10:17:29 -0500

    added to functions and lists

---
+++ function.py
@@ -0,0 +1,19 @@
+def hello_world():
+    print("hello world")
+    hello_world()
+    def add(a,b):
+        print(a + b)
+    def subtract(a,b):
+        print(a-b)
+    def multiply(a,b):  
+        print(a*b)
+    def divide(a,b):
+        print(a/b)
+    def remainder(a,b):
+        print(a%b)
+        # add, subtract, multiply, divide and remainder
+        x= add(9,10)
+        print(x)
+    def slope(m,x,b):
+        return m*x+b 
+    y= slope (9,10,20)
--- lists.py
+++ lists.py
@@ -1,5 +1,6 @@
 my_list = [ "cars","sports","food","music","excercises"]
 print(my_list)
+# creation of my first list
 my_list.append("drinks")
 print(my_list)
 print(my_list[3])
@@ -11,3 +12,14 @@ print(my_list)
 my_tuple = ("pizza","hotdogs","tacos","steak","soup")
 # creating my tuple list
 print(my_tuple)
+print(len(my_list))
+my_list.sort()
+print(my_list)
+my_list.reverse()
+print(my_list)
+second_list = ["apples","bananas","oranges","peaches","tangerines"]
+# creation of my second list
+print(my_list + second_list)
+print(">".join(my_list + second_list))
+names= "anna,wesley,saint".split(",")
+print(names)
