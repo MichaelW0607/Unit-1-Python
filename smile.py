ee2ca6bff07e94d7b6f83f6d5f9c8fefec98a1f8
MichaelW0607 <113442619+MichaelW0607@users.noreply.github.com>
2022-11-23 10:11:15 -0500

    added smiley face

---
+++ smile.py
@@ -0,0 +1,24 @@
+from turtle import *
+
+def eye():
+    begin_fill()
+    circle(25)
+    end_fill()
+
+
+begin_fill()
+color("yellow") 
+circle(100)
+end_fill()
+color("green")
+left(90)
+forward(100)
+left(90)
+forward(50)
+left(180)
+
+
+eye()
+
+forward(100)
+done()
