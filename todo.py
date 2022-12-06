1fa909b36852ea18ff50ae12b8728ec20714a0e0
MichaelW0607 <113442619+MichaelW0607@users.noreply.github.com>
2022-12-06 10:46:30 -0500

    text

--- todo.py
+++ todo.py
@@ -1,10 +1,10 @@
 todos = []
-x= input("What to add to list?")
-todos.append(x)
-print("Your todo list is")
-print(todos) 
 while True:
- print("What to add to list?")
-x = ""
-print("your todo list is ")
-x = input("What to add to list?")
+    print("your todos are:")
+    print(todos)
+    x=input("what todo would you like to add?")
+    todos.append(x)
+    my_todo = open["todo.txt", "a"]
+    my_todo.write(x,"\n")
+    my_todo.close()
+    my_todo = open("todo.txt")
