2aae98dda79179e0f6b6fa78070810480dc9c79b
MichaelW0607 <113442619+MichaelW0607@users.noreply.github.com>
2022-12-07 10:25:25 -0500

    errors

---
+++ try.py
@@ -0,0 +1,16 @@
+try:
+ age = int(input('Enter your age: '))
+
+ faveNum = int(input('What is your favorite number: '))
+ if age <= 21:
+  print('You are not allowed to enter, you are too young.')
+ else:
+  print('Welcome, you are old enough.')
+
+ print("Fun Fact! Your age divided by your favorite number is: " , age / faveNum)
+except ValueError:
+ print("Please input a number")
+except ZeroDivisionError:
+ print("Please do not enter the number zero")
+finally:
+    print("This code runs no matter what")
